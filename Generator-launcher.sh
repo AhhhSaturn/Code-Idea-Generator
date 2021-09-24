2d43d4fbdbfa91d1075a7dccbb738ceae3cd6215
Doordedoor0 <67267844+doordedoor0@users.noreply.github.com>
2021-09-24 19:38:49 +0100

    made it go yeeeeeeeeeeeeeeet

--- Generator-launcher.sh
+++ Generator-launcher.sh
@@ -1,6 +1,9 @@
 var=`$((RANDOM))`
 if [ $var = 1 ]
 then
-  genmes=Genorating good idea
+  genmes="Genorating good idea"
+elif [ $var = 2 ]
+then
+  genmes="You want idea? I give idea."
 fi
-zenity --progress --pulsate --title="Code idea generator" --text="$genmes"
+sleep 2 | zenity --progress --pulsate --title="Code idea generator" --text="$genmes" --auto-close
